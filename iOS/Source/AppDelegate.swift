cec1caf0b337fe6b4189f624bf182710e1a14955
Elvis Nuñez <elvisnunez@me.com>
2016-08-27 07:39:32 +0200

    Use localization

--- iOS/Source/AppDelegate.swift
+++ iOS/Source/AppDelegate.swift
@@ -7,10 +7,16 @@ class AppDelegate: UIResponder {
 
 extension AppDelegate: UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        print(NSLocale.currentLocale().localeIdentifier)
+
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         guard let window = self.window else { fatalError("Window not found") }
 
-        window.rootViewController = RootController()
+        let controller = RootController()
+        controller.title = NSLocalizedString("Breakfast", comment: "")
+
+        let navigationController = UINavigationController(rootViewController: controller)
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
 
         return true
