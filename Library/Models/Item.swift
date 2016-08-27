50cbf7636765877b6233ce7ace2082d5e774f838
Elvis Nuñez <elvisnunez@me.com>
2016-08-27 23:33:49 +0200

    Display items

--- Library/Models/Item.swift
+++ Library/Models/Item.swift
@@ -1,60 +1,58 @@
 import Foundation
 
-enum PageType {
-    case breakfast, lunch, dinner, others
-}
-
-protocol Item {
-    var title: String { get }
-    var price: Double { get }
-    var isSeparator: Bool { get }
-}
-
-struct BreakfastItem: Item {
-    enum Type {
-        case platosListos, desayuno, sandwichs, jugos
+struct BreakfastItem {
+    enum GroupType: String {
+        case platosListos = "PLATOS LISTOS"
+        case desayuno = "DESAYUNO"
+        case sandwichs = "SANDWICHS"
+        case jugos = "JUGOS"
     }
 
     let title: String
     let price: Double
     let isSeparator: Bool
-    let type: Type
 
-    static func generateData() -> [BreakfastItem] {
-        var items = [BreakfastItem]()
+    static func generateData() -> [String : [BreakfastItem]] {
+        var items = [String : [BreakfastItem]]()
 
         let platosListosPath = NSBundle.mainBundle().pathForResource("desayuno-platos-listos", ofType: "plist")!
-        let platosListos = NSArray(contentsOfFile: platosListosPath) as? [[String : AnyObject]] ?? [[String : AnyObject]]()
-        for dictionary in platosListos {
+        let importedPlatosListos = NSArray(contentsOfFile: platosListosPath) as? [[String : AnyObject]] ?? [[String : AnyObject]]()
+        var platosListos = [BreakfastItem]()
+        for dictionary in importedPlatosListos {
             guard let title = dictionary["title"] as? String else { continue }
             guard let price = dictionary["price"] as? Double else { continue }
 
-            let item = BreakfastItem(title: title, price: price, isSeparator: false, type: .platosListos)
-            items.append(item)
+            let item = BreakfastItem(title: title, price: price, isSeparator: false)
+            platosListos.append(item)
         }
+        items[BreakfastItem.GroupType.platosListos.rawValue] = platosListos
 
-        let item = BreakfastItem(title: "", price: 0, isSeparator: false, type: .desayuno)
-        items.append(item)
+        let item = BreakfastItem(title: "", price: 0, isSeparator: false)
+        items[BreakfastItem.GroupType.desayuno.rawValue] = [item]
 
         let sandwichsPath = NSBundle.mainBundle().pathForResource("sandwichs", ofType: "plist")!
-        let sandwichs = NSArray(contentsOfFile: sandwichsPath) as? [[String : AnyObject]] ?? [[String : AnyObject]]()
-        for dictionary in sandwichs {
+        let importedSandwichs = NSArray(contentsOfFile: sandwichsPath) as? [[String : AnyObject]] ?? [[String : AnyObject]]()
+        var sandwichs = [BreakfastItem]()
+        for dictionary in importedSandwichs {
             guard let title = dictionary["title"] as? String else { continue }
             guard let price = dictionary["price"] as? Double else { continue }
 
-            let item = BreakfastItem(title: title, price: price, isSeparator: false, type: .sandwichs)
-            items.append(item)
+            let item = BreakfastItem(title: title, price: price, isSeparator: false)
+            sandwichs.append(item)
         }
+        items[BreakfastItem.GroupType.desayuno.rawValue] = sandwichs
 
         let jugosPath = NSBundle.mainBundle().pathForResource("jugos", ofType: "plist")!
-        let jugos = NSArray(contentsOfFile: jugosPath) as? [[String : AnyObject]] ?? [[String : AnyObject]]()
-        for dictionary in jugos {
+        let importedJugos = NSArray(contentsOfFile: jugosPath) as? [[String : AnyObject]] ?? [[String : AnyObject]]()
+        var jugos = [BreakfastItem]()
+        for dictionary in importedJugos {
             guard let title = dictionary["title"] as? String else { continue }
             guard let price = dictionary["price"] as? Double else { continue }
 
-            let item = BreakfastItem(title: title, price: price, isSeparator: false, type: .jugos)
-            items.append(item)
+            let item = BreakfastItem(title: title, price: price, isSeparator: false)
+            jugos.append(item)
         }
+        items[BreakfastItem.GroupType.jugos.rawValue] = jugos
 
         return items
     }
--- iOS/Source/ListController.swift
+++ iOS/Source/ListController.swift
@@ -6,6 +6,38 @@ class ListController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return data.keys.count
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.objectsForSection(section).count
+    }
+
+    func objectsForSection(section: Int) -> [BreakfastItem] {
+        switch section {
+        case 0:
+            return data[BreakfastItem.GroupType.platosListos.rawValue] ?? [BreakfastItem]()
+        case 1:
+            return data[BreakfastItem.GroupType.desayuno.rawValue] ?? [BreakfastItem]()
+        case 2:
+            return data[BreakfastItem.GroupType.sandwichs.rawValue] ?? [BreakfastItem]()
+        case 3:
+            return data[BreakfastItem.GroupType.jugos.rawValue] ?? [BreakfastItem]()
+        default:
+            return [BreakfastItem]()
+        }
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        let items = self.objectsForSection(indexPath.section)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
+
+        return cell
     }
 }
