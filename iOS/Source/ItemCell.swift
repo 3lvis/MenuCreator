b12fa47275ae7305acc78fd4f91744c13e2de10e
Elvis Nuñez <elvisnunez@me.com>
2016-08-28 09:09:34 +0200

    Style cells

--- iOS/Source/ItemCell.swift
+++ iOS/Source/ItemCell.swift
@@ -5,7 +5,8 @@ class ItemCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
         self.contentView.addSubview(self.titleLabel)
-        self.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        self.contentView.addSubview(self.subtitleLabel)
+        self.contentView.addSubview(self.arrowView)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -18,9 +19,24 @@ class ItemCell: UITableViewCell {
         return label
     }()
 
+    lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .Right
+
+        return label
+    }()
+
+    lazy var arrowView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "arrow")
+
+        return view
+    }()
+
     var item: BreakfastItem? {
         didSet {
             self.titleLabel.text = item?.title
+            self.subtitleLabel.text = String(item!.price)
         }
     }
 
@@ -29,13 +45,35 @@ class ItemCell: UITableViewCell {
 
         let containerWidth = self.frame.width
         let containerHeight =  self.frame.height
+        self.contentView.frame = CGRect(x: 0, y: 0, width: containerWidth, height: containerHeight)
+
 
         let horizontalMargin = CGFloat(20)
-        let width = containerWidth - (horizontalMargin * 2)
+        let boundsWidth = containerWidth - (horizontalMargin * 2)
 
         var titleLabelFrame: CGRect {
+            let width = boundsWidth * 0.85
             return CGRect(x: horizontalMargin, y: 0, width: width, height: containerHeight)
         }
         self.titleLabel.frame = titleLabelFrame
+
+        let accessoryWidth = self.arrowView.image?.size.width ?? 0
+
+        var detailLabelFrame: CGRect {
+            let width = boundsWidth * 0.13 - accessoryWidth
+            let x = self.titleLabel.frame.maxX
+            return CGRect(x: x, y: 0, width: width, height: containerHeight)
+        }
+        self.subtitleLabel.frame = detailLabelFrame
+
+        var accessoryViewFrame: CGRect {
+            let accessoryHeight = self.arrowView.image?.size.height ?? 0
+            let rightMargin = CGFloat(15)
+            let x = self.subtitleLabel.frame.maxX + rightMargin
+            let y = (containerHeight - accessoryHeight) / 2
+            return CGRect(x: x, y: y, width: accessoryWidth, height: accessoryHeight)
+        }
+        print(accessoryViewFrame)
+        self.arrowView.frame = accessoryViewFrame
     }
 }
