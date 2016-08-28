30be8baa8ec3cd4dae19ccf825745bfe95b4a634
Elvis Nuñez <elvisnunez@me.com>
2016-08-28 12:46:49 +0200

    Add sections

--- iOS/Source/Item/ItemController.swift
+++ iOS/Source/Item/ItemController.swift
@@ -7,6 +7,23 @@ protocol ItemControllerDelegate: class {
 }
 
 class ItemController: UITableViewController {
+    enum Section: Int {
+        case title, price, others, delete
+
+        func toString() -> String? {
+            switch self {
+            case .title:
+                return NSLocalizedString("Title", comment: "")
+            case .price:
+                return NSLocalizedString("Price", comment: "")
+            case .others:
+                return NSLocalizedString("Others", comment: "")
+            case .delete:
+                return nil
+            }
+        }
+    }
+
     weak var controllerDelegate: ItemControllerDelegate?
     var item: BreakfastItem
     var isNewItem = false
@@ -34,6 +51,35 @@ class ItemController: UITableViewController {
         self.title = self.isNewItem ? NSLocalizedString("Creating", comment: "") : NSLocalizedString("Editing", comment: "")
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Save, target: self, action: #selector(save))
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: #selector(cancel))
+        self.tableView.register(UITableViewCell)
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return ItemController.Section.count
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(UITableViewCell.reuseIdentifier, forIndexPath: indexPath)
+        cell.textLabel?.text = "Hi"
+
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Title"
+        case 1:
+            return "Price"
+        case 2:
+            return "Others"
+        default:
+            return nil
+        }
     }
 
     func save() {
